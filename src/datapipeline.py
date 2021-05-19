69a1b1c53239147ebb6e7543cb3d51bbfafd93db
mbalakiran <balakiranmanthri1995@gmail.com>
2021-05-19 19:53:39 +0200

    readconfig

--- src/datapipeline.py
+++ src/datapipeline.py
@@ -3,14 +3,22 @@ import argparse
 import yaml
 import logging
 
+def read_config(config_path):
+    with open(config_path) as yaml_f:
+        config = yaml.safe_load(yaml_f)
+    return config
 
 
+def main(config_path, datasource):
+    config= read_config(config_path)
+    print(config)
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default="default")
+    defa = os.path.join("config","params.yaml")
+    args.add_argument("--config", default= defa)
     args.add_argument("--datasource",default=None)
 
     parsed_args = args.parse_args()
-    print(parsed_args.config, parsed_args.datasource)
+    main(config_path=parsed_args.config, datasource=parsed_args.datasource)
+    # print(parsed_args.config, parsed_args.datasource)
